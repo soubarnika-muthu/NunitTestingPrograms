fb6872e298f060ec380b5093dfee1fd11e9f0d8d
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-02 19:23:22 +0530

    [Soubarnika]Computation of Square Root Program

--- NunitTestingPrograms/Program.cs
+++ NunitTestingPrograms/Program.cs
@@ -2,50 +2,56 @@
 
 namespace NunitTestingPrograms
 {
+    /// P1- Genrating changes for the given amount in vending machine
+    /// P2- Computing the date to display the day using Gregorian calendar formula
+    /// P3- Converts the temperature as celsius to fahrenheit and vice verse
+    /// P4- Calculating the monthly payment 
+    /// P5- Calculating square root of the number
+    /// </summary>
     class Program
     {
-        /// P1- Genrating changes for the given amount in vending machine
-        /// P2- Computing the date to display the day using Gregorian calendar formula
-        /// P3- Converts the temperature as celsius to fahrenheit and vice verse
-        /// P4- Calculating the monthly payment 
-        /// </summary>
-        
-            static void Main(string[] args)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to N Unit Testing Programs");
+            Console.WriteLine("1 - Vending Machine");
+            Console.WriteLine("2 - Day of week");
+            Console.WriteLine("3 - Temperature Conversion");
+            Console.WriteLine("4 - Monthly Payment");
+            Console.WriteLine("5 - Square Root");
+            Console.WriteLine("Enter you choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
             {
-                Console.WriteLine("Welcome to N Unit Testing Programs");
-                Console.WriteLine("1 - Vending Machine");
-                Console.WriteLine("2 - Day of week");
-                Console.WriteLine("3 - Temperature Conversion");
-                Console.WriteLine("4 - Monthly Payment");
-                Console.WriteLine("Enter you choice:");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        VendingMachine vendingMachine = new VendingMachine();
-                        vendingMachine.ReadInput();
-                        break;
+                case 1:
+                    VendingMachine vendingMachine = new VendingMachine();
+                    vendingMachine.ReadInput();
+                    break;
 
-                    case 2:
-                        DayOfWeek dayOfWeek = new DayOfWeek();
-                        dayOfWeek.ReadInput();
-                        break;
+                case 2:
+                    DayOfWeek dayOfWeek = new DayOfWeek();
+                    dayOfWeek.ReadInput();
+                    break;
 
-                    case 3:
-                        TemperaturConversion temperaturConversion = new TemperaturConversion();
-                        temperaturConversion.ReadInput();
-                        break;
+                case 3:
+                    TemperaturConversion temperaturConversion = new TemperaturConversion();
+                    temperaturConversion.ReadInput();
+                    break;
 
-                    case 4:
-                        MonthlyPayment.ReadInput();
-                        break;
+                case 4:
+                    MonthlyPayment.ReadInput();
+                    break;
 
-                    default:
-                        break;
-                }
-                Console.Read();
+                case 5:
+                    SquareRoot.ReadInput();
+                    break;
+
+                default:
+                    break;
             }
+            Console.Read();
         }
     }
+}
+
 
 
---
+++ NunitTestingPrograms/SquareRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTestingPrograms
+{
+    class SquareRoot
+    {
+        const double EPSILON = 1e-15;
+        public static void ReadInput()
+        {
+            Console.WriteLine("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            CalculateSqrt(number);
+
+        }
+
+        private static void CalculateSqrt(int num)
+        {
+            double temp = num;
+            do
+            {
+                temp = (temp + (num / temp)) / 2;
+
+            } while (Math.Abs(temp - num / temp) > (EPSILON * temp));
+
+            Console.WriteLine("The square root of {0} is {1}", num, temp);
+        }
+    }
+}
