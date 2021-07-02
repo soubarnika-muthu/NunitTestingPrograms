f835b494e5b4e34a8f6c4a8e01d238d07dd48ec3
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-02 19:03:06 +0530

    [Soubarnika]Computation of Vending Machine Program

--- NunitTestingPrograms/Program.cs
+++ NunitTestingPrograms/Program.cs
@@ -4,9 +4,28 @@ namespace NunitTestingPrograms
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        // <summary>
+        /// P1- Genrating changes for the given amount in vending machine
+        /// </summary>
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to N Unit Testing Programs");
+                Console.WriteLine("1 - Vending Machine");
+                Console.WriteLine("Enter you choice:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        VendingMachine vendingMachine = new VendingMachine();
+                        vendingMachine.ReadInput();
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.Read();
+            }
         }
     }
-}
+
---
+++ NunitTestingPrograms/VendingMachine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTestingPrograms
+{
+    class VendingMachine
+    {
+        public readonly int[] NOTES = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
+        public void ReadInput()
+        {
+            Console.WriteLine("Welcome to Vending Machine");
+            Console.WriteLine("Enter the Amount Given:");
+            //reading input from the user
+            int amount = Convert.ToInt32(Console.ReadLine());
+            GeneratinBalance(amount);
+        }
+
+        //method for generating the change
+        private void GeneratinBalance(int amt)
+        {
+
+            int[] noteCounter = new int[NOTES.Length];
+            int numberOfNotes = 0;
+
+            for (int iter = 0; iter < NOTES.Length; iter++)
+            {
+                //checking whether the amount is greater than current change
+                if (amt >= NOTES[iter])
+                {
+                    noteCounter[iter] = amt / NOTES[iter];
+                    amt -= NOTES[iter] * noteCounter[iter];  // store the balance amount after the change
+                    numberOfNotes += noteCounter[iter];
+                    // if amount becomes 0 then exit the loop
+                    if (amt < 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Minimum number of notes :" + numberOfNotes);
+            //printing the changes
+            for (int iter = 0; iter < NOTES.Length; iter++)
+            {
+                if (noteCounter[iter] != 0)
+                {
+                    Console.WriteLine("{0} of {1}", noteCounter[iter], NOTES[iter]);
+                }
+            }
+
+
+        }
+    }
+}
